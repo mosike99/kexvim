d4c5b47ad29aa3e437097c2bbb9d7604da50ce1f
Ubuntu <ubuntu@localhost.localdomain>
2026-07-27 20:27:30 +0800

    feat: 单文件 sage.js (280KB)，移除 tsup chunk

--- dist/EntityStore-A2QHCJNC.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-OALBSDTT.js";export{a as SQLiteEntityStore};
--- dist/EntityStore-UGP4OMTS.js
+++
@@ -1,6 +0,0 @@
-import {
-  SQLiteEntityStore
-} from "./chunk-NIL2H2P7.js";
-export {
-  SQLiteEntityStore
-};
